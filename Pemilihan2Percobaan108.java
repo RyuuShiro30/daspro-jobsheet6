706b915b4676d6e0421e10090913e388a7a97ce5
RyuuShiro30 <annoraegaprasetyo>
2024-10-01 12:11:16 +0700

    second-commit

--- Pemilihan2Percobaan108.java
+++ Pemilihan2Percobaan108.java
@@ -10,14 +10,21 @@ public class Pemilihan2Percobaan108 {
         System.out.println("tahun: ");
         tahun = input.nextInt();
 
-        if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0) {
-                System.out.println("tahun kabisat");
-
+        if (tahun %4 == 0) {
+            if (tahun %100 == 0) {
+                if (tahun %400 == 0) {
+                    System.out.println("tahun kabisat");
+                    
+                } else {
+                    System.out.println("bukan tahun kabisat");
+                }
+                
             } else {
-                System.out.println("bukan tahun kabisat");
+                System.out.println("tahun kabisat");
             }
 
+        } else {
+            System.out.println("bukan tahun kabisat");
         }
     }
 }
